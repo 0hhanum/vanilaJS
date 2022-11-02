42fd6659a570dad5c4c4058c217b15da8a7034c2
hanum <rntls123@naver.com>
2022-11-03 02:07:24 +0900

    하반기 데브매칭 2트

---
+++ coffee copy/screens/ProductDetailPage.js
@@ -0,0 +1,114 @@
+import Router from "../Router.js";
+import { fetchProduct } from "../api.js";
+
+const parsePrice = (price) => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+async function ProductDetailPage() {
+  const id = window.location.pathname.split("/").at(-1);
+  const { imageUrl, name, price, productOptions } = await fetchProduct(id);
+  const state = {
+    cart: [],
+    total: 0,
+  };
+  const DOMText = `
+        <div class="ProductDetailPage">
+            <h1>${name} 상품 정보</h1>
+            <div class="ProductDetail">
+                <img src="${imageUrl}">
+                <div class="ProductDetail__info">
+                <h2>${name}</h2>
+                <div class="ProductDetail__price">${parsePrice(price)}원~</div>
+                <select>
+                    <option>선택하세요.</option>
+                    ${productOptions
+                      .map((productOption) => {
+                        return productOption.stock === 0
+                          ? `
+                        <option value=${productOption.id} disabled>(품절) ${name} ${productOption.name}</option>
+                        `
+                          : productOption.price === 0
+                          ? `
+                        <option value=${productOption.id}>${name} ${productOption.name}</option>
+                        `
+                          : `
+                        <option value=${productOption.id}>${name} ${
+                              productOption.name
+                            } (+${parsePrice(productOption.price)}원)</option>
+                        `;
+                      })
+                      .join("")}
+                </select>
+                <div class="ProductDetail__selectedOptions">
+                    <h3>선택된 상품</h3>
+                    <ul>
+                    </ul>
+                    <div class="ProductDetail__totalPrice">0원</div>
+                    <button class="OrderButton">주문하기</button>
+                </div>
+                </div>
+            </div>
+        </div>    
+    `;
+  const parser = new DOMParser();
+  const DOM = parser.parseFromString(DOMText, "text/html").querySelector("div");
+  const cartDOM = DOM.querySelector("ul");
+  const selectDOM = DOM.querySelector("select");
+  const totalPriceDOM = DOM.querySelector(".ProductDetail__totalPrice");
+  selectDOM.addEventListener("change", (e) => {
+    const value = e.target.value;
+    if (value === "선택하세요") return;
+    const option = productOptions.find((productOption) => {
+      return productOption.id === parseInt(value);
+    });
+    if (state.cart.find((c) => c.id === value)) return;
+    state.cart.push({ id: value, amount: 1, optionId: option.id });
+    const cartItemDOMText = `
+        <li>
+        ${name} ${option.name} <div><input min="0" max="${option.stock}" type="number" value="1">개</div>
+        </li>        
+        `;
+    const cartItemDOM = parser
+      .parseFromString(cartItemDOMText, "text/html")
+      .querySelector("li");
+    const inputDOM = cartItemDOM.querySelector("input");
+    inputDOM.addEventListener("change", (e) => {
+      const inputVal = e.target.value.trim();
+      if (inputVal === "") return;
+      let amount = parseInt(inputVal);
+      if (amount > option.stock) {
+        inputDOM.value = option.stock;
+        amount = option.stock;
+      } else if (amount < 0) {
+        inputDOM.value = 0;
+        amount = 0;
+      }
+      const currentState = state.cart.find((c) => c.id === value);
+      const amountDiff = amount - currentState.amount;
+      currentState.amount = amount;
+      state.total += (option.price + price) * amountDiff;
+      totalPriceDOM.innerHTML = `${parsePrice(state.total)}원`;
+    });
+    state.total += option.price + price;
+    cartDOM.appendChild(cartItemDOM);
+    totalPriceDOM.innerHTML = `${parsePrice(state.total)}원`;
+  });
+  DOM.querySelector("button").addEventListener("click", () => {
+    const currentCartItem =
+      JSON.parse(localStorage.getItem("products_cart")) || [];
+    const newCartItem = [];
+    state.cart.forEach((c) => {
+      newCartItem.push({
+        productId: c.id,
+        optionId: c.optionId,
+        quantity: c.amount,
+      });
+    });
+    localStorage.setItem(
+      "products_cart",
+      JSON.stringify([...currentCartItem, ...newCartItem])
+    );
+  });
+  return DOM;
+}
+export default ProductDetailPage;
--- coffee/screens/CartPage.js
+++ coffee/screens/CartPage.js
@@ -1,27 +1,42 @@
-<div class="CartPage">
-  <h1>장바구니</h1>
-  <div class="Cart">
-    <ul>
-      <li class="Cart__item">
-        <img
-          src="https://grepp-cloudfront.s3.ap-northeast-2.amazonaws.com/programmers_imgs/assignment_image/cafe_coffee_cup.png"
-        />
-        <div class="Cart__itemDesription">
-          <div>커피잔 100개 번들 10,000원 10개</div>
-          <div>100,000원</div>
+import Router from "../Router.js";
+function CartPage() {
+  const cartItems = JSON.parse(localStorage.getItem("products_cart"));
+  console.log(cartItems);
+  if (cartItems.length === 0 || !cartItems) {
+    alert("장바구니가 비어있습니다");
+    history.pushState({}, null, `/web/`);
+    Router(true);
+    return;
+  }
+  const DOMText = `
+    <div class="CartPage">
+    <h1>장바구니</h1>
+    <div class="Cart">
+        <ul>
+        
+        </ul>
+        <div class="Cart__totalPrice">
         </div>
-      </li>
-      <li class="Cart__item">
-        <img
-          src="https://grepp-cloudfront.s3.ap-northeast-2.amazonaws.com/programmers_imgs/assignment_image/cafe_coffee_cup.png"
-        />
-        <div class="Cart__itemDesription">
-          <div>커피잔 1000개 번들 15,000원 5개</div>
-          <div>75,000원</div>
+        <button class="OrderButton">주문하기</button>
         </div>
-      </li>
-    </ul>
-    <div class="Cart__totalPrice">총 상품가격 175,000원</div>
-    <button class="OrderButton">주문하기</button>
-  </div>
-</div>
+    </div>    
+    `;
+  const parser = new DOMParser();
+  const DOM = parser.parseFromString(DOMText, "text/html").querySelector("div");
+  const ul = DOM.querySelector("ul");
+  cartItems.forEach((item) => {
+    const txt = `
+        <li class="Cart__item">
+            <img src="${item.imageUrl}">
+            <div class="Cart__itemDesription">
+            <div>커피잔 100개 번들 10,000원 10개</div>
+            <div>100,000원</div>
+            </div>
+        </li>
+        `;
+  });
+  const button = DOM.querySelector("button");
+  const totalDOM = DOM.querySelector(".Cart__totalPrice");
+  return DOM;
+}
+export default CartPage;
