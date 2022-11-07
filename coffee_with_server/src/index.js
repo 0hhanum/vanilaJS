9030e4561f671f8820fa2a371ff088b065862bf2
hanum <rntls123@naver.com>
2022-11-07 21:32:50 +0900

    Nodemon

--- coffee_with_server/src/index.js
+++ coffee_with_server/src/index.js
@@ -1,8 +1,1 @@
-import Router from "./Router.js";
-
-console.log("start app");
-
-window.onload = () => {
-  Router(true);
-};
-window.addEventListener("popstate", () => Router());
+console.log("test");
