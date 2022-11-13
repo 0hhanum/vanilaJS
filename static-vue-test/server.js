86af0c03f1b075905e38a980e3cfdb63ee9fcdc2
hanum <rntls123@naver.com>
2022-11-14 00:26:32 +0900

    test vue in static page

---
+++ static-vue-test/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+app.use("/src", express.static(path.resolve(__dirname, "src")));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("src", "index.html"));
+});
